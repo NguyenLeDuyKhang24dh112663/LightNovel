169bd8f938d821e85bfa11b5744d4246ddbbb2a7
NguyenLeDuyKhang24dh112663 <nubbieim982@gmail.com>
2026-03-10 20:24:13 +0700

    tiểu thuyết lightnovel

--- app/src/main/java/com/example/lightnovel/ProfileSectionFragment.kt
+++ app/src/main/java/com/example/lightnovel/ProfileSectionFragment.kt
@@ -1,10 +1,6 @@
 package com.example.lightnovel
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 class ProfileSectionFragment : Fragment(R.layout.fragment_profile) {
     
